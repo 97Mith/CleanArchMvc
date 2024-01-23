a1010bf65034318a56a1e83ec657de3f4b65d29f
97Mith <136820655+97Mith@users.noreply.github.com>
2024-01-23 17:52:22 -0300

    first

---
+++ CleanArchMvc/CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,34 @@
+﻿using CleanArchMvc.Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Entities
+{
+	public sealed class Category
+	{
+        public int Id { get; private set; }
+		public string Name { get; private set; }
+
+        public Category(string name)
+        {
+            Name = name;
+        }
+        public Category(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        //define que uma categoria pode ter uma coleção de produtos
+        //afinal uma categoria pode tem n produtos
+        ICollection<Product> Products { get; set; }
+
+        private void ValidationDomain(string name)
+        {
+            DomainValidationException.When(string.IsNullOrEmpty(name), "The name is invalid, cannot be null");
+        }
+    }
+}
